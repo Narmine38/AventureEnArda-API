964c2f2488f9646b15eadf358d8eb40bbd79bd48
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-10-16 23:36:01 +0200

    - Mise a jour de la table password_reset_tokens

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -7,22 +7,26 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Cette méthode est exécutée lors de la migration.
+     * Elle crée la table 'password_reset_tokens' pour gérer les tokens de réinitialisation de mot de passe.
      */
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            // Champs pour gérer la réinitialisation de mot de passe
+            $table->id(); // Ceci créera une clé primaire auto-incrémentée nommée 'id'
+            $table->string('email')->primary(); // L'email de l'utilisateur servant de clé primaire
+            $table->string('token'); // Le token généré pour la réinitialisation de mot de passe
+            $table->timestamp('created_at')->nullable(); // La date et l'heure de création du token pour éventuellement vérifier sa validité
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Cette méthode est exécutée pour annuler la migration.
+     * Elle supprime la table 'password_reset_tokens'.
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('password_reset_tokens'); // Supprime la table 'password_reset_tokens' si elle existe
     }
 };
