1a7f9f488400daeb6c2f5d04142c04ce2ed58944
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-10-25 09:55:43 +0200

    - Mise a jour de CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://aventure-en-adra.netlify.app'],
+    'allowed_origins' => ['https://aventure-en-adra.netlify.app','http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
