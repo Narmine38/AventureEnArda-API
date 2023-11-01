abbb7f7787faf5305429441d8dbf7b9c95ea3bac
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-02 00:45:15 +0100

    - Mise a jour de kernel et sanctum

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
