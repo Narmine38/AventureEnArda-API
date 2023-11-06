b257577061eedbe282f37b5233458dc54cda9d13
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-06 15:33:53 +0100

    - Ajout du model Activites

---
+++ app/Models/Activite.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Activite extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'picture',
+        'type',
+        'age_range',
+        'place_id',
+    ];
+
+
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
+}
